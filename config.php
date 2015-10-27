04390ad480dd25a8e9127021e13cd7dc8963d422
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2015-10-27 05:03:36 -0200

    Adição de Arquivos
    Adicionados index.php e config.php (arquivos em branco) na raiz.

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Arquivo config.php
+ *
+ * @author Gabriel Bertola Bocca <gabriel at estudiodigitalbocca.com.br>
+ * 
+ */
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    
+    /**
+     * 
+     */
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>

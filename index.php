b1dff5915c63d8efeb210db6a4bdf339a3f235ae
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2015-10-28 10:05:13 -0200

    Atualização
    config.php agora está implementado e a index.php usa um exemplo de
    implementação

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
     
-    /**
-     * 
-     */
-
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="ISO-8859-1">
-        <title></title>
-    </head>
-    <body>
-        <?php
+    require_once 'config.php';
+    
+    var_dump($_GET['slug']);
+    
+    if(isset($_GET['slug'])){
+        /**
+         * Cria uma nova instancia da URLAmigavel
+         * $url = new \EstudioDigitalBocca\URLAmigavel\URLAmigavel($_GET['slug']);
+         *
+         * Remove a barra do final da url amigavel para a pesquisa no banco
+         * $removeBarra = new \EstudioDigitalBocca\URLAmigavel\RemovedorDeBarra($url);
+         *
+         * Para obter o valor correto e usar na sua pesquisa no banco de dados,
+         * basta usar o método:
+         * $url->obterValor();
+         */
         
-        ?>
-    </body>
-</html>
+        /**
+         * Modelo de implementação
+         */
+        
+        $url = new \EstudioDigitalBocca\URLAmigavel\URLAmigavel($_GET['slug']);
+        
+        echo "<pre>";
+        echo "Valor Passado no Endere&ccedil;o: ";
+        echo $url->obterValor();
+        echo "</pre>";
+        
+        $removeBarra = new \EstudioDigitalBocca\URLAmigavel\RemovedorDeBarra($url);
+        
+        echo "<pre>";
+        echo "Valor Para a Pesquisa no Banco: ";
+        echo $url->obterValor();
+        echo "</pre>";
+        
+        echo "<pre>";
+        echo "var_dump do valor final: ";
+        echo var_dump($url->obterValor());
+        echo "</pre>";
+
+    }

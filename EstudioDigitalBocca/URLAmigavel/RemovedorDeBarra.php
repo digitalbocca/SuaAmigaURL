4fb01ba4734cc60e45484291c4dd2ba62f9a4dca
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-03-08 19:06:37 -0300

    Alteracoes no Layout

--- EstudioDigitalBocca/URLAmigavel/RemovedorDeBarra.php
+++ EstudioDigitalBocca/URLAmigavel/RemovedorDeBarra.php
@@ -3,49 +3,65 @@
 namespace EstudioDigitalBocca\URLAmigavel;
 
 /**
- * Remove a barra do final da string se existir
+ * Remove a barra do in√≠cio e do final da string se existir
  *
  * @author Gabriel Bertola Bocca <gabriel at estudiodigitalbocca.com.br>
  */
 class RemovedorDeBarra {
-    
+
     private $valor = '';
     private $ultimaPosicao = '';
-    
-    private function removeBarra(){
+    private $primeiraPosicao = '';
+
+    private function removeBarraInicial(){
+        $this->valor = substr($this->valor, 1);
+    }
+
+    private function removeBarraFinal(){
         $this->valor = substr($this->valor, 0, -1);
     }
-    
-    private function verificarBarra(){
+
+    private function verificarBarraFinal(){
         if($this->ultimaPosicao == '/' ){
-            $this->removeBarra();
+            $this->removeBarraFinal();
         }
     }
-    
+
+    private function verificarBarraInicial(){
+        if($this->primeiraPosicao == '/' ){
+            $this->removeBarraInicial();
+        }
+    }
+
     private function atualizaValor(AtualizarValor $objeto){
         $objeto->atualizarValor($this->valor);
     }
-    
+
     private function extraiUltimaPosicao(){
         $this->ultimaPosicao = substr($this->valor, -1);
     }
-    
+
+    private function extraiPrimeiraPosicao(){
+        $this->primeiraPosicao = substr($this->valor, 0, 1);
+    }
+
     /**
      * Recebe um objeto que implementa a Interface ObterValor
-     * e remove a barra do final da string obtida pelo metodo obterValor()
-     * quando houver necessidade.
-     * 
-     * Caso n„o haja necessidade a string do objeto permanece a mesma sem
-     * nenhum prejuizo e sem disparar nenhuma excess„o.
-     * 
+     * e remove a barra do in√≠cio e do final da string obtida pelo metodo
+     * obterValor() quando houver necessidade.
+     *
+     * Caso n√£o haja necessidade a string do objeto permanece a mesma sem
+     * nenhum prejuizo e sem disparar nenhuma excess√£o.
+     *
      * @param \EstudioDigitalBocca\URLAmigavel\ObterValor $objeto
      *     Objeto que implementa a Interface ObterValor
      */
     public function __construct(ObterValor $objeto){
         $this->valor = $objeto->obterValor();
         $this->extraiUltimaPosicao();
-        $this->verificarBarra();
+        $this->extraiPrimeiraPosicao();
+        $this->verificarBarraFinal();
+        $this->verificaBarraInicial();
         $this->atualizaValor($objeto);
-        //Acima h· um erro, pois atualiza o valor mesmo se estiver sem a barra.
     }
 }
--- somente.php
+++ somente.php
@@ -1,43 +1,41 @@
 <?php
-    
+
     require_once 'config.php';
-    
+
+    //echo phpinfo();
+
+    echo $_SERVER['REQUEST_URI'];
+
     echo "<pre>";
     echo "GET que foi recebido pela p&aacute;gina: ";
     var_dump($_GET['slug']);
     echo "</pre>";
-    
+
     if(isset($_GET['slug'])){
+
         /**
          * Cria uma nova instancia da URLAmigavel
          * $url = new \EstudioDigitalBocca\URLAmigavel\URLAmigavel($_GET['slug']);
-         *
-         * Remove a barra do final da url amigavel para a pesquisa no banco
-         * $removeBarra = new \EstudioDigitalBocca\URLAmigavel\RemovedorDeBarra($url);
          *
          * Para obter o valor correto e usar na sua pesquisa no banco de dados,
          * basta usar o método:
          * $url->obterValor();
          */
-        
-        /**
-         * Modelo de implementação
-         */
-        
+
         $url = new \EstudioDigitalBocca\URLAmigavel\URLAmigavel($_GET['slug']);
-        
+
         echo "<pre>";
         echo "Valor Passado no Endere&ccedil;o: ";
         echo $url->obterValor();
         echo "</pre>";
-        
+
         $removeBarra = new \EstudioDigitalBocca\URLAmigavel\RemovedorDeBarra($url);
-        
+
         echo "<pre>";
         echo "Valor Para a Pesquisa no Banco: ";
         echo $url->obterValor();
         echo "</pre>";
-        
+
         echo "<pre>";
         echo "var_dump do valor final: ";
         echo var_dump($url->obterValor());
